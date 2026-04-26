ae788b815595adedecb66c219cb39e95460ea447
George Sandu <georgesandumail@icloud.com>
2026-04-26 17:10:08 +0300

    Implement HardwareManager and debug trigger simulation (#148, #149)

--- P2PShopping/P2PShopping/P2PShoppingApp.swift
+++ P2PShopping/P2PShopping/P2PShoppingApp.swift
@@ -12,13 +12,19 @@ import WebKit
 struct P2PShoppingApp: App {
     
     @StateObject private var locationManager = LocationPermissionManager()
+    @StateObject private var hardwareManager = HardwareManager.shared
+    
+    // Flag pentru a ne asigura că testul rulează o singură dată
+    @State private var hasRunDebugTest = false
     
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(locationManager)
+                .environmentObject(hardwareManager)
                 .onAppear {
                     setupBridge()
+                    initializeHardware()
                 }
         }
     }
@@ -27,5 +33,22 @@ struct P2PShoppingApp: App {
         let contentController = WKUserContentController()
         let bridgeHandler = LocationBridgeHandler(permissionManager: locationManager)
         contentController.add(bridgeHandler, name: "locationBridge")
+    }
+    
+    private func initializeHardware() {
+        hardwareManager.locationManager = locationManager // Legătura necesară pentru Task #34
+        hardwareManager.initialize()
+        
+        #if DEBUG
+        if !hasRunDebugTest {
+            print("P2PShoppingApp: Running Debug test trigger for Task #149...")
+            hardwareManager.handleHardwareTrigger(
+                storeId: "debug_store_001",
+                itemId: "debug_item_999",
+                triggerType: "BUTTON_PRESS"
+            )
+            hasRunDebugTest = true
+        }
+        #endif
     }
 }
